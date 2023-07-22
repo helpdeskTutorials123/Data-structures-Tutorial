67ba68a24cf0387552de76f7f1038b2018dbd97e
helpdeskTutorials123 <140255301+helpdeskTutorials123@users.noreply.github.com>
2023-07-22 19:04:26 +0530

    Update Linkdlist.py

--- Linkdlist.py
+++ Linkdlist.py
@@ -1,40 +1,30 @@
-# Linked List implementation WITHOUT insert_at_end() FUNCTION
+# This function is in LinkedList class.
+# Inserts a new node after the given prev_node. This method is
+# defined inside LinkedList class shown above */
+def insertAfter(self, prev_node, new_data):
 
-# Activity 01 Implement Node class in python
-class Node: 
-    def __init__(self, dataval): 
-        self.dataval = dataval 
-        self.next = None
+	# 1. check if the given prev_node exists
+	if prev_node is None:
+		print("The given previous node must inLinkedList.")
+		return
 
+	# 2. Create new node &
+	# 3. Put in the data
+	new_node = Node(new_data)
 
-# Activity 02 Create another LinkedList class using the above Node class.
-class LinkedList: 
-    def __init__(self): 
-        self.head = None 
-        
-    def insert_at_head(self, dataval): 
-        new_node = Node(dataval) 
-        new_node.next = self.head 
-        self.head= new_node 
+	# 4. Make next of new Node as next of prev_node
+	new_node.next = prev_node.next
 
-    def traverse(self): 
-        n = self.head 
-        while n is not None: 
-            print(n.dataval, end=' ') 
-            n = n.next
+	# 5. make next of prev_node as new_node
+	prev_node.next = new_node
 
+# Data insert into end of a linkdlist
 
-# Activity 03 implement the function insert_at_end() in the LinkedList class
 class LinkedList:
     def __init__(self):
         self.head = None
         
-    def insert_at_head(self, dataval):
-        new_node = Node(dataval)
-        new_node.next = self.head
-        self.head = new_node
-        
-    def insert_at_end(self, dataval):
+    def at_end(self, dataval):
         new_node = Node(dataval)
         if self.head is None:
             self.head = new_node
@@ -44,16 +34,11 @@ class LinkedList:
             last = last.next
         last.next = new_node
         
-    def traverse(self):
-        n = self.head
-        while n is not None:
-            print(n.dataval, end=' ')
-            n = n.next
 
 
 # Create actual liked list
 my_list = LinkedList()
-my_list.insert_at_end(1)
-my_list.insert_at_end(2)
-my_list.insert_at_end(3)
+my_list.at_end(1)
+my_list.at_end(2)
+my_list.at_end(3)
 my_list.traverse()  # Output: 1 2 3
